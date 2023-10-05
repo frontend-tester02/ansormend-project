a89e4d3a506a09249fabdcee03c377405afa3b3e
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2023-10-05 23:36:27 +0900

    Make site header responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const elSiteHeader = document.querySelector('.site-header');
+const elSiteHeaderToggleButton = elSiteHeader.querySelector('.site-header-toggle-button');
+
+
+if (elSiteHeaderToggleButton) {
+  elSiteHeaderToggleButton.addEventListener('click', function () {
+    elSiteHeader.classList.toggle('site-header--open');
+
+  });
+
+}
